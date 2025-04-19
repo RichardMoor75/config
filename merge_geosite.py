bfd081a3696f838c77d408a6a60c7b8373f1b0a5
RichardMoor75 <144960257+RichardMoor75@users.noreply.github.com>
2025-04-19 12:25:21 +0300

    Update merge_geosite.py

--- merge_geosite.py
+++ merge_geosite.py
@@ -1,3 +1,4 @@
+import os
 import geosite_pb2
 
 def read_geosite(file_path):
@@ -14,11 +15,11 @@ def merge_geosites(original, custom):
     for site in custom.entry:
         for d in site.domain:
             if (d.type, d.value) not in seen:
-                # Добавляем в первую группу
                 original.entry[0].domain.append(d)
                 seen.add((d.type, d.value))
 
 def write_geosite(db, file_path):
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
     with open(file_path, 'wb') as f:
         f.write(db.SerializeToString())
 
